4201e51f733fba0d32d6d46010d71c2c9ae4f42b
svtter <svtter@qq.com>
2025-06-10 11:04:41 +0800

    update host and model.

--- src/config.py
+++ src/config.py
@@ -1,6 +1,5 @@
 skip_models = ["mobileswr*", "CRNN"]
 
-pi_host = "http://192.168.2.18:8000"
-local_host = "http://127.0.0.1:8000"
-
-test_host = local_host
+pi_host = "http://192.168.2.18:8000"  # for pi
+local_host = "http://127.0.0.1:8000"  # for local (server)
+test_host = pi_host
