e50dedcca4da7d6ee51a67bb2b956dba971d4b3f
svtter <svtter@qq.com>
2025-06-10 11:40:46 +0800

    update readme, project name, and config file.

---
+++ README.md
@@ -0,0 +1,10 @@
+# Embed Validator
+
+通过 [openapi.json](./openapi.json) 定义的接口，测试运行在服务器端模型的性能。
+
+执行测试：
+
+````bash
+PYTHONPATH=. uv run inference_client.py
+```
+````
--- src/config.py
+++ src/config.py
@@ -1,5 +1,8 @@
-skip_models = ["mobileswr*", "CRNN"]
+skip_models = [
+  # "mobileswr*",
+  # "CRNN",
+]
 
 pi_host = "http://192.168.2.18:8000"  # for pi
 local_host = "http://127.0.0.1:8000"  # for local (server)
-test_host = pi_host
+test_host = local_host
